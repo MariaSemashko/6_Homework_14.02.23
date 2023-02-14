596bc2c34a7e4ce5aa6154531612b7f9994cdb70
Maria <maria.a.semashko@gmail.com>
2023-02-14 08:59:30 +0300

    Task 1, Task 2

---
+++ Task001/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int [] GetArray(string message)
+{
+    Console.WriteLine(message);
+    string str = Console.ReadLine();
+    string[] strArray = str.Split(',');
+
+    int[] intArray = new int[strArray.Length];
+    
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        intArray[i] = Convert.ToInt32(strArray[i]);
+    }
+    return intArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int CountPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+int [] arr = GetArray("Введите целые числа через запятую: ");
+PrintArray(arr);
+
+
+Console.WriteLine($"Количество чисел больше 0 равно {CountPositive(arr)}");
---
+++ Task002/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double GetNumber(string message)
+{
+    double result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+(double, double) GetCoordinate(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+   
+    return (x, y);
+}
+
+double b1 = GetNumber("Введите значение b1");
+double k1 = GetNumber("Введите значение k1");
+double b2 = GetNumber("Введите значение b2");
+double k2 = GetNumber("Введите значение k2");
+
+(double x, double y) = GetCoordinate(k1, b1, k2, b2);
+Console.WriteLine($"Точка пересечения двух прямых, заданных уравнениями y = k1*x + b1, y = k2*x + b2 ({x}; {y})");
